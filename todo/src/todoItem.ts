f7c7fcba55befaf9f89e7f35658ba058e509d353
Nicolas TOISON <nicolas@MacBook-Air-de-Nicolas.local>
2024-02-17 10:40:32 +0100

    refactor(todo-app): refactor TodoItem to more concise code

--- todo/src/todoItem.ts
+++ todo/src/todoItem.ts
@@ -1,15 +1,15 @@
 export class TodoItem {
-  public id: number;
-  public task: string;
-  public isComplete: boolean = false;
-
-  public constructor(id: number, task: string, isComplete: boolean = false) {
+  constructor(
+    public id: number,
+    public task: string,
+    public isComplete: boolean = false
+  ) {
     this.id = id;
     this.task = task;
     this.isComplete = isComplete;
   }
 
-  public printDetails(): void {
+  printDetails(): void {
     console.log(
       `$(this.id)\t${this.task} ${this.isComplete ? "\t(complete)" : ""}`
     );
